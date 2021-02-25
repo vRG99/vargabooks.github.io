ba811a1ece0c6c2d2b293e7e6f15d97cce27dc8c
K2KVWN <varga.andras1999@gmail.com>
2021-02-25 17:06:00 +0100

    VRG_Project_Generator program created (empty)

---
+++ VRG_Projects_Source_Generator/VRG_Projects_Source_Generator/Generator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VRG_Projects_Source_Generator
+{
+    public class Generator
+    {
+        // authors: author_id, first_name, last_name, birth_date, country_of_birth
+
+
+        // isbn: book_title, date_of_publication, length, list_price, category_id, author_id, publisher_id
+
+
+        // categories: category_id, category_name
+
+
+        // customers: customer_id, first_name, middle_name, last_name, gender, address_id, email_address, phone_number
+
+
+        // purchases: purch_row_id, purch_id, store_id, isbn, customer_id, purchase_quantity, purchase_price_per_piece, purchase_date
+
+
+        // stocks: stock_id, store_id, isbn, quantity
+
+
+
+
+
+    }
+}
---
+++ VRG_Projects_Source_Generator/VRG_Projects_Source_Generator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VRG_Projects_Source_Generator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
